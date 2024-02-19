6d469ea5da71479fb4f97a3fad0bab9a9ad8ea54
KirilApostolov <kirilapostolov@gmail.com>
2024-02-19 14:09:51 +0200

    NOI 3 E

--- dream.cpp
+++ dream.cpp
@@ -5,7 +5,7 @@ int main(){
     int n, t = 0; cin >> n; int b[n];
     for(int i = 0; i < n; i++) cin >> b[i];
     for(int i = 0; i < n - 3; i++)
-        for(int j = i + 2; j < n -1; j++)
+        for(int j = i + 2; j < n - 1; j++)
             t = max(t, b[i] + b [i+1] + b[j] + b[j+1]);
     cout << t;
 	return 0;
---
+++ shopping.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n, d, r = 1; cin >> n >> d;
+    long long p[n], m = 0, s, ind;
+    for(int i = 0; i < n; i++) cin >> p[i];
+    for(int i = 0; i < n; i++){
+        s = 0;
+        for(int j = 0; j < n; j++) {
+            ind = (i + j) % n;
+            s += p[ind];
+            if (s % d == 0 && m < s) {
+                m = s;
+                r = j + 1;
+            }
+        }
+    }
+    cout << r;
+    return 0;
+}
